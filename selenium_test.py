ab0889e9be44dc8d78d70064a8a467de19d7fd20
ABIMANYU72 <abifire0410@gmail.com>
2025-03-19 15:03:39 +0530

    Updated test script and report

--- selenium_test.py
+++ selenium_test.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
 
